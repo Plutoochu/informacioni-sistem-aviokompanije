7227c2d32b22705a462d357ab5b7a442ca5efee7
nmilavica1 <nmilavica1@etf.unsa.ba>
2025-04-06 21:21:36 +0200

    Dodan UI za reset passworda

--- client/src/AppRuter.jsx
+++ client/src/AppRuter.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Pocetna from "./glavne-komponente/Pocetna";
 import "./stilovi/App.css";
+import ResetPassword from "./glavne-komponente/ResetPassword";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Pocetna />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
     </BrowserRouter>
   );
