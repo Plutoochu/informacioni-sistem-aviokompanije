e7183a098f0ac370b836fff0dbe3ddcb5f5a1aff
aomeragic1 <aomeragic1@etf.unsa.ba>
2025-04-06 22:19:03 +0200

    Dodana forma za destinacije.

--- client/src/AppRuter.jsx
+++ client/src/AppRuter.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Pocetna from "./glavne-komponente/Pocetna";
 import "./stilovi/App.css";
+import DestinacijeForma from "./glavne-komponente/DestinacijeForma";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Pocetna />} />
+        <Route path="/destinacije" element={<DestinacijeForma />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/glavne-komponente/Pocetna.jsx
+++ client/src/glavne-komponente/Pocetna.jsx
@@ -39,12 +39,16 @@ const Pocetna = () => {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
       <Dugme text="Klik za provjeru!" onClick={provjeraHandler} />
       {poruka !== null && (
         <p>
