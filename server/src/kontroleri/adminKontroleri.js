73e52c3f75f9c23d3e341284baa16647783efd2e
adlakic <adlakic1@etf.unsa.ba>
2025-04-07 13:57:21 +0200

    Dodane destinacije i implementirana logika za dodavanje

--- server/src/kontroleri/adminKontroleri.js
+++ server/src/kontroleri/adminKontroleri.js
@@ -1,3 +1,36 @@
+import fs from 'fs';
+import path from 'path';
+import { Destinacija } from "../modeli.js";
+
+const destinacijeJsonPath = path.join(__dirname, '..', 'destinacije.json');
+
+const ucitajDestinacijeIzJsonFajla = () => {
+  const rawData = fs.readFileSync(destinacijeJsonPath);
+  return JSON.parse(rawData);
+};
+
+const unesiDestinacijeIzJsonFajla = async () => {
+  const destinacije = ucitajDestinacijeIzJsonFajla();
+  
+  try {
+    await Destinacija.insertMany(destinacije);
+    console.log('Destinacije su uspješno unesene!');
+  } catch (err) {
+    console.log('Greška pri unosu destinacija u bazu', err);
+  }
+};
+
 export const provjeraAplikacije = (req, res) => {
   return res.status(200).json({ poruka: "Sve ok!" });
 };
+
+const unosDestinacijaIzJsonFajla = async (req, res) => {
+  try {
+    await unesiDestinacijeIzJsonFajla();
+    res.status(200).json({ poruka: "Destinacije uspješno unesene iz JSON fajla!" });
+  } catch (err) {
+    res.status(500).json({ poruka: "Greška pri unosu destinacija." });
+  }
+};
+
+export { unosDestinacijaIzJsonFajla };
--- server/src/rute/adminRute.js
+++ server/src/rute/adminRute.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { provjeraAplikacije } from "../kontroleri/adminKontroleri.js";
+import { provjeraAplikacije, unosDestinacijaIzJsonFajla } from "../kontroleri/adminKontroleri.js";
 import { proslijediDalje } from "../middlewares.js";
 
 const router = Router();
 
+// Ruta za provjeru aplikacije
 router.get("/provjera", proslijediDalje, provjeraAplikacije);
+
+// Ruta za unos destinacija iz JSON fajla
+router.post("/unesi-destinacije-json", unosDestinacijaIzJsonFajla);
 
 export default router;
